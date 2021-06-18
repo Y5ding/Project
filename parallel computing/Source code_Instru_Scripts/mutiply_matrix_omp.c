328f9e4b280e2d8f420fad6e637dd79666aa7934
Y5ding <y5ding@ucsd.edu>
2021-06-17 23:17:05 -0700

    Create mutiply_matrix_omp.c

---
+++ parallel computing/Source code_Instru_Scripts/mutiply_matrix_omp.c
@@ -0,0 +1,60 @@
+#include <math.h>
+#include <omp.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "timer.h"
+
+#define NN 1024
+#define NM 1024
+
+double A[NN][NM];
+double B[NN][NM];
+double C[NN][NM];
+
+int main(int argc, char** argv)
+{
+
+  const int n = NN;
+  const int m = NM;
+  const int max = 1000;
+  int nthreads, procs,
+
+  printf("Matrix Multiplication: %d x %d mesh\n", n, m);
+
+  StartTimer();
+#pragma omp parallel for shared(m, n, A, B)
+{
+  for(i = 0; i < m; ++i){
+    for(j = 0; j < n; ++j){
+       A[i][j] = rand() % max;
+    }      //matrix A
+  }
+  for(p = 0; p < m; ++p){
+    for(q = 0; q < n; ++q){
+       B[p][q] = rand() % max;
+    }      //matrix B
+  }
+}
+
+#pragma omp parallel for shared(m, n, A, B) private(nthreads)
+{
+  procs = omp_get_num_procs();
+  nthreads = omp_get_num_threads();
+  for(s = 0; s < m; ++s)
+  {
+    for(t = 0; t < n; ++t)
+    {
+      C[s][t]=0;
+      for(k = 0; k < m; ++k)
+      C[s][t] += A[s][k] * B[k][t];
+    }
+  }
+  printf("Number of processors = %d\n", procs);
+  printf("Number of threads = %d\n", nthreads);
+}
+
+    double runtime = GetTimer();
+
+    printf(" total: %f s\n", runtime / 1000);
+}
